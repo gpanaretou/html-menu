fa2f4e41e83b66bb42ea7390e2c55245233cea97
George Panaretou <gpanaretou@hotmail.com>
2024-08-07 00:36:44 +0300

    change color palette, add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       fontFamily: {
         edu: ["edu", "serif"],
       },
+    colors: {
+      'tan': '#f1ddbf',
+      'forest-purple': '#525e75',
+      'forest-purple': '#524275',
+    }
     },
   },
   plugins: [],
